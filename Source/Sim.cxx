0a2d5bb7c907c217d6931a9ae202f8e22a93bc07
Shevtsov Kirill <shevtsov.km@phystech.edu>
2026-04-05 01:30:22 +0300

    Sim for slae

---
+++ Source/Sim.cxx
@@ -0,0 +1,17 @@
+#include "Slae.hxx"
+/*sim for sparse matrix: v[i + 1] = p @ v[i]  + b*/
+std::pair<Vector, double> SimpleIteration(const SparseMatrix& p, const Vector& b, const Vector& vBegin, std::size_t iter, double tol){
+    Vector v = vBegin;
+    double delta = Norm(v);
+    for(std::size_t l = 0; l < iter; ++l){
+        Vector current = p * v + b;
+        double e = Norm(current);
+        if(e < tol){
+            return {Vector(v), e};
+        }
+        delta = e;
+        v = current;
+    }
+    return {v, delta};
+}
+[[nodiscard]] Vector Solve(const SparseMatrix& mtx, const Vector& b, const Vector& xBegin){}

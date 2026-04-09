174a1fe9a15cbb91b7e33ca86c158ade185ceb78
Shevtsov Kirill <shevtsov.km@phystech.edu>
2026-04-10 01:17:15 +0300

    added Tn method

--- Source/Sim.cxx
+++ Source/Sim.cxx
@@ -1,4 +1,5 @@
 #include "Slae.hxx"
+#include <cmath>
 struct IterationArgs{
     SparseMatrix a_;
     Vector b_;
@@ -82,4 +83,46 @@ std::pair<Vector, double> SimpleIteration(const IterationArgs& args){
     }
     delta = EuclidNorm(mtx * v - b);
     return {v, delta};
+}
+/*iter = 2^k, k - is std::size_t*/
+[[nodiscard]] std::pair<Vector, double> Tn(const SparseMatrix& mtx, const Vector& b,
+                                const Vector& xBegin, std::size_t iter, double tolerance) {
+    std::size_t r = std::log(iter) / std::log(2.0);
+    std::vector<std::size_t> idx = {0};
+    for(std::size_t i = 0; i < r; ++i) {
+        std::size_t m = std::pow(2, i);
+        
+        std::vector<std::size_t> c;
+        for(const auto& k : idx) {
+            c.push_back(k);
+            c.push_back(2 * m - k - 1);
+        }
+        idx = c;
+    }
+    /*create Chebyshov polynomial roots*/
+    auto lambda = [&] (std::size_t n, std::size_t s) -> double {
+        double arg = std::numbers::pi_v<double> * (2 * s + 1) / (2 * n);
+        return std::cos(arg);
+    };
+
+    std::vector<double> tau(iter);
+    for(std::size_t i = 0; i < iter; ++i) {
+        tau[i] = lambda(iter, idx[i] );
+    }
+
+    Vector v = xBegin;
+    double delta = EuclidNorm(mtx * v - b);
+    for(std::size_t l = 0; l < iter; ++l){
+        Vector vCurrent = v - (mtx * v - b) * tau[l];
+        /*current r = Ax - b*/
+        double error = EuclidNorm(mtx * vCurrent - b);
+        if(error < tolerance){
+            double absoluteDelta = error;
+            return {vCurrent, absoluteDelta};
+        }
+        delta = error;
+        v = vCurrent;
+    }
+    double absoluteDelta = delta;
+    return {v, absoluteDelta};
 }
